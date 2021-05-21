c69dd184d1c738f6e6c12cf8856de8f8418aa1b4
thatretrodev <thatretrodev@outlook.com>
2021-05-20 22:46:43 -0400

    Change some more stuff

--- build.sh
+++ build.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+if [ -d bin ]
+then
+	rm -r bin/*
+else
+	mkdir bin
+fi
+
+
 # Compile the Emulator
 cd emulator
 go build
+mv emulator ../bin
 cd ..
 
 # Compile the Assembler
 cd assembler
 go build
+mv assembler ../bin
 cd ..
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/thatretrodev/retrolikevirtualcomputer
-
-go 1.14
